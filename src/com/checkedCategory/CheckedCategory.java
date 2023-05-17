b609f4a71c91809786ee747501bb967539739af0
nwarbade <nwarbade@gmail.com>
2023-05-17 18:07:39 +0530

    Initial commit

---
+++ src/com/checkedCategory/CheckedCategory.java
@@ -0,0 +1,20 @@
+package com.checkedCategory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class CheckedCategory {
+	
+public static void main(String[] args) throws FileNotFoundException {
+		
+		
+		String path = "F:\\Revision\\";
+		
+		FileInputStream fis = new FileInputStream(path);
+		
+		System.out.println("last line");
+		
+		
+	}
+
+}
